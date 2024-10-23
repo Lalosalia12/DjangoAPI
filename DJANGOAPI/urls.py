8848dbaa66b8dfe712fab8ed44b38e9df50cd2ae
Lalosalia12 <2021451044@teschi.edu.mx>
2024-10-23 03:32:33 -0600

    Avancevideo5

--- DJANGOAPI/urls.py
+++ DJANGOAPI/urls.py
@@ -17,14 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from api.login.login_view import (
-    login_views,registrar_views,recuperar_views,
-    
-    )
+    login_views, registrar_views, recuperar_views,
+    salir_view,
+)
 from api.home.home_views import home_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', login_views,name='login'),
-    path('registrar/', registrar_views,name='registrar'),
-    path('recuperar/', recuperar_views,name='recuperar'),
-    path('', home_views,name='home'),
+    path('login/', login_views, name='login'),
+    path('salir/', salir_view, name='salir'),
+    path('registrar/', registrar_views, name='registrar'),
+    path('recuperar/', recuperar_views, name='recuperar'),
+    path('', home_views, name='home'),
+    path('home/', home_views, name='home'),
 ]
--- api/login/login_view.py
+++ api/login/login_view.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 def login_views(request):
     template_name = "login.html"
     
-    return render(request,template_name)
+    # Verifica si el usuario ya está autenticado
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, template_name, {'error': 'Credenciales inválidas'})
+    
+    return render(request, template_name)
+
+
 #view for register
 def registrar_views(request):
     template_name = "registrar.html"
@@ -15,3 +33,8 @@ def recuperar_views(request):
     template_name = "recuperar.html"
     
     return render(request,template_name)
+
+#view for salir view
+def salir_view(request):
+    logout(request)
+    return redirect('login')
